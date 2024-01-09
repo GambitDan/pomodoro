b7776956296391dfc9bde1381f204e4ace4eac3e
Daniel Houghton <danielhoughton1986@gmail.com>
2024-01-09 21:41:47 +0000

    start button action implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -15,9 +16,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
+def start_timer():
+    count_down(5 * 60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -38,9 +46,10 @@ timer_text = canvas.create_text(
 )
 canvas.grid(column=1, row=1)
 
-count_down(5)
 
-start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW)
+start_button = Button(
+    text="Start", highlightthickness=0, highlightbackground=YELLOW, command=start_timer
+)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW)
